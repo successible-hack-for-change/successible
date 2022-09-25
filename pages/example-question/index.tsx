cff6917c7cf7e393066891bec0ea8a6fb14e6982
IssyYeung <issyyeung@hotmail.co.uk>
2022-09-25 18:07:42 +0100

    Created instructions and example question pages

---
+++ pages/example-question/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import PageLayout from '../PageLayout';
+
+const ExampleQuestion: NextPage = () => {
+  return (
+    <PageLayout>
+      <h1>Example question page</h1>
+      <p>This is where we explain how to do our test.</p>
+    </PageLayout>
+  );
+};
+
+export default ExampleQuestion;
