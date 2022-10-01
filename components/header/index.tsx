e3065c34f963fcf21cccde16024ab1fc344a766f
IssyYeung <issyyeung@hotmail.co.uk>
2022-10-01 19:40:09 +0100

    Navbar header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Navbar, Button } from '@blueprintjs/core';
+import Link from 'next/link';
 
 const Header = (): JSX.Element => {
   return (
     <header>
-      <p>Place holder header</p>
+      <Navbar>
+        <Navbar.Group>
+          <Navbar.Heading>Successible</Navbar.Heading>
+          <Navbar.Divider />
+          <Link href="/">
+            <Button className="bp4-minimal" icon="home" text="Home" />
+          </Link>
+          <Link href="/instructions">
+            <Button className="bp4-minimal" icon="manual" text="Instructions" />
+          </Link>
+          <Link href="/example-question">
+            <Button
+              className="bp4-minimal"
+              icon="learning"
+              text="Example question"
+            />
+          </Link>
+          <Link href="/example-question">
+            <Button className="bp4-minimal" icon="play" text="Start test" />
+          </Link>
+        </Navbar.Group>
+      </Navbar>
     </header>
   );
 };
