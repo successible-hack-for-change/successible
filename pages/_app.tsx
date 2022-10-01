6bff400a400790d5280be45537d807fb31bc5f14
IssyYeung <issyyeung@hotmail.co.uk>
2022-10-01 19:12:49 +0100

    Installed and implemented Blueprint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import '@blueprintjs/popover2/lib/css/blueprint-popover2.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
