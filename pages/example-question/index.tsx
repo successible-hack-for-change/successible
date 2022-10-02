a6f4cdb027514628a0d825f86baff0c4d4e26d74
IssyYeung <issyyeung@hotmail.co.uk>
2022-10-02 20:47:52 +0100

    Added question to example question page

--- pages/example-question/index.tsx
+++ pages/example-question/index.tsx
@@ -1,13 +1,31 @@
 import type { NextPage } from 'next';
 import PageLayout from '../PageLayout';
 import OptionalExtras from '../../components/optionalExtras';
-import { H1 } from '@blueprintjs/core';
+import { Radio, RadioGroup } from '@blueprintjs/core';
 
 const ExampleQuestion: NextPage = () => {
   return (
     <PageLayout>
-      <H1>Example question page</H1>
-      <p>This is where we explain how to do our test.</p>
+      <h1>Example question page</h1>
+      <div className="flex gap-5 bg-light rounded-lg p-5">
+        <div className="flex-1">
+          <h3>Question:</h3>
+          <p>
+            If Daves Corner Shop had 16 employees in 2020 and and 21 employees
+            in 2022 what percentage increase of employees was there between 2020
+            and 2022?
+          </p>
+        </div>
+        <div className="flex-1 bg-lightest rounded-lg px-3">
+          <h3>Answers:</h3>
+          <RadioGroup onChange={() => {}}>
+            <Radio label="110% increase" />
+            <Radio label="14.7% increase" />
+            <Radio label="31.25% increase" />
+            <Radio label="20% increase" />
+          </RadioGroup>
+        </div>
+      </div>
       <OptionalExtras />
     </PageLayout>
   );
