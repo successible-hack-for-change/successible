bcabe602de629371fbdfc10b2e7fddb9d925a47f
IssyYeung <issyyeung@hotmail.co.uk>
2022-10-02 19:11:59 +0100

    Added temporary colours to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
+    colors: {
+      black: '#000000',
+      white: '#ffffff',
+      grey: {
+        lighest: '#f8f9fa',
+        light: '#dee2e6',
+        DEFAULT: '#adb5bd',
+        dark: '#495057',
+        darkest: '#212529',
+      },
+      lightest: '#f2e9e4',
+      light: '#c9ada7',
+      mid: '#9a8c98',
+      dark: '#4a4e69',
+      darkest: '#22223b',
+      accent: {
+        accentLight: '#FFCAD4',
+        DEFAULT: '#C1666B',
+        accentDark: '#913B3F',
+      },
+    },
   },
   plugins: [],
 };
