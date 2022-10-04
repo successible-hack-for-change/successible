37bfbf240dd3a5518e4e9f5c522049bc80891eb2
Beth Gooding <beth.gooding@hotmail.com>
2022-10-04 20:24:11 +0100

    Creates test-in-progress page

---
+++ pages/test-in-progress/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import PageLayout from '../PageLayout';
+
+const TestInProgress: NextPage = () => {
+  return (
+    <PageLayout>
+      <h1>Test in progress</h1>
+    </PageLayout>
+  );
+};
+
+export default TestInProgress;
