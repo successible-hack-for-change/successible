4c77aa96caeb5ae9bacd7bcef68708859958fc57
IssyYeung <issyyeung@hotmail.co.uk>
2022-10-23 22:58:54 +0100

    Completed page

--- pages/completed/index.tsx
+++ pages/completed/index.tsx
@@ -1,10 +1,25 @@
 import { NextPage } from 'next';
 import PageLayout from '../PageLayout';
+import Image from 'next/image';
 
 const Completed: NextPage = () => {
   return (
     <PageLayout>
-      <h1>Completed</h1>
+      <div className="p-4 max-w-4xl flex-col justify-center items-center text-center mx-auto my-10">
+        <Image
+          src="/successible-logo.svg"
+          alt="Successible logo"
+          height={140}
+          width={140}
+        />
+        <h1 className="p-5">Congratulations</h1>
+        <p>
+          You have reached the end of your test and your answers have been
+          sumbitted. Your employers will contact you directly with the next
+          steps of your application.
+        </p>
+        <p>You may now close the page.</p>
+      </div>
     </PageLayout>
   );
 };
