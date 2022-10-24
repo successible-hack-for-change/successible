3a04bc9ccda50e3e8dcbe53a0b265af2cbe00010
IssyYeung <issyyeung@hotmail.co.uk>
2022-10-24 09:25:01 +0100

    Added logo to footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -4,16 +4,17 @@ import Image from 'next/image';
 const Footer = (): JSX.Element => {
   return (
     <footer className="p-4 bg-darkest w-full text-white mt-auto">
-      <div className="mx-auto">
-        <p className="text-center">
-          To contact Successible, email us at info@successible.com
-        </p>
-        <p className="text-center">
-          Powered by{' '}
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </p>
+      <div className="flex items-center justify-center">
+        <Image
+          src="/successible-logo-white.svg"
+          alt="Successible logo"
+          height={40}
+          width={40}
+        />
+        <div className="pl-3">
+          <p className="p-0 m-0 text-base">Successible</p>
+          <p className="p-0 m-0 text-xs">One test for everyone</p>
+        </div>
       </div>
     </footer>
   );
