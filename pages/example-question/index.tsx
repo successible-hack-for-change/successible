c3c5c37fbc78ee1752e026b7711d09d5fbdfdffd
IssyYeung <issyyeung@hotmail.co.uk>
2022-10-24 20:44:12 +0100

    Testing to see if spinner fixes tour errors in prod

--- pages/example-question/index.tsx
+++ pages/example-question/index.tsx
@@ -2,30 +2,34 @@ import type { NextPage } from 'next';
 import PageLayout from '../PageLayout';
 import QuestionDisplay from '../../components/questionDisplay';
 import Tour from '../../components/tour/Tour';
+import { Suspense } from 'react';
+import { Spinner } from '@blueprintjs/core';
 
 const ExampleQuestion: NextPage = () => {
   return (
-    <PageLayout>
-      <QuestionDisplay
-        id={0}
-        question="If Daves Corner Shop had 16 employees in 2020 and and 21 employees
+    <Suspense fallback={<Spinner />}>
+      <PageLayout>
+        <QuestionDisplay
+          id={0}
+          question="If Daves Corner Shop had 16 employees in 2020 and and 21 employees
           in 2022 what percentage increase of employees was there between 2020
           and 2022?"
-        answer={'C'}
-        resA={'110% increase'}
-        resB={'14.7% increase'}
-        resC={'31.25% increase'}
-        resD={'20% increase'}
-        highlight={"Sometimes I hide because I'm shy."}
-        image={
-          "I like to be center of attention so everything else should be closed when I'm open."
-        }
-        definition={"I'm just here to watch the fight."}
-        timeLimit={60}
-        handleSubmitOnClick={() => {}}
-      />
-      <Tour />
-    </PageLayout>
+          answer={'C'}
+          resA={'110% increase'}
+          resB={'14.7% increase'}
+          resC={'31.25% increase'}
+          resD={'20% increase'}
+          highlight={"Sometimes I hide because I'm shy."}
+          image={
+            "I like to be center of attention so everything else should be closed when I'm open."
+          }
+          definition={"I'm just here to watch the fight."}
+          timeLimit={60}
+          handleSubmitOnClick={() => {}}
+        />
+        <Tour />
+      </PageLayout>
+    </Suspense>
   );
 };
 
