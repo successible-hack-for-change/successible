81535d929c4c5c2c6404e75743ddc8134689a4c3
Beth Gooding <beth.gooding@hotmail.com>
2022-10-30 20:42:37 +0000

    Removes unused variables from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,8 @@ import '@blueprintjs/popover2/lib/css/blueprint-popover2.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import AppContext from '../context/AppContext';
-import { Question } from '../interfaces/questionTypes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [questions, setQuestions] = useState<Question[]>([]);
   const [userId, setUserId] = useState<number>(0);
   return (
     <AppContext.Provider
