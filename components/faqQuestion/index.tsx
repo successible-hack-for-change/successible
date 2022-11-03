fae088d365b9169daa2a55df52a0ed4a0db8f25c
IssyYeung <issyyeung@hotmail.co.uk>
2022-11-03 23:22:22 +0000

    Added FAQ page

---
+++ components/faqQuestion/index.tsx
@@ -0,0 +1,23 @@
+import { Button, Collapse, Pre } from '@blueprintjs/core';
+import React, { useState } from 'react';
+
+interface FaqQuestionInterface {
+  question: string;
+  answer: string;
+}
+
+const FaqQuestion = ({
+  question,
+  answer,
+}: FaqQuestionInterface): JSX.Element => {
+  return (
+    <details className="mb-2">
+      <summary className="bg-light rounded-md p-3 mb-1 text-lg font-medium">
+        {question}
+      </summary>
+      <p className="bg-lightest rounded-md p-3 mb-0 mx-2">{answer}</p>
+    </details>
+  );
+};
+
+export default FaqQuestion;
