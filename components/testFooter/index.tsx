e84e8451ca7f6b78805158a7ee6043a6a1c6973e
IssyYeung <issyyeung@hotmail.co.uk>
2022-11-03 23:33:38 +0000

    Made non-interactive footer for test page

---
+++ components/testFooter/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Image from 'next/image';
+
+const TestFooter = (): JSX.Element => {
+  return (
+    <footer className="p-4 bg-darkest w-full text-white mt-auto">
+      <div className="flex items-center justify-center">
+        <Image
+          src="/successible-logo-white.svg"
+          alt="Successible logo"
+          height={40}
+          width={40}
+        />
+        <div className="pl-3">
+          <p className="p-0 m-0 text-base">Successible</p>
+          <p className="p-0 m-0 text-xs">Opportunity for everyone</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default TestFooter;
