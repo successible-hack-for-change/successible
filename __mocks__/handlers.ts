fa989265bc10c1a9b0e3e3ae6c7f98459263680f
Beth Gooding <beth.gooding@hotmail.com>
2022-11-09 20:43:35 +0000

    Rewrite tests in StartTest.tsx to remove causing API calls

--- __mocks__/handlers.ts
+++ __mocks__/handlers.ts
@@ -2,14 +2,21 @@ import { rest } from 'msw';
 
 export const handlers = [
   // Handles a POST /users request
-  rest.post('/users', (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        //some correct information
-      }),
-    );
-  }),
+  rest.post(
+    'https://successible-api-nqnaexycua-nw.a.run.app/users',
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          id: 1,
+          username: 'testuser@email.com',
+          email: 'testuser@email.com',
+          score: 0,
+          accessCode: 'demo',
+        }),
+      );
+    },
+  ),
 
   // Handles a GET / request
   rest.get('/', (req, res, ctx) => {
