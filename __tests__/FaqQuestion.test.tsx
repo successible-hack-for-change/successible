e992e404ac4045528fb11d6e99e58cd59787b436
Beth Gooding <beth.gooding@hotmail.com>
2022-11-09 21:32:45 +0000

    Adds unit tests for the FaqQuestion page

---
+++ __tests__/FaqQuestion.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import FaqQuestion from '../components/faqQuestion';
+
+test('It should display an FAQ with the given question and answer props', () => {
+  render(<FaqQuestion question="question" answer="answer" />);
+  expect(screen.getByText('question')).toBeInTheDocument();
+  expect(screen.getByText('answer')).toBeInTheDocument();
+});
