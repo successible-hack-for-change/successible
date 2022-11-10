35250c3a23baa86e368966f2b619e353bdf78395
Beth Gooding <beth.gooding@hotmail.com>
2022-11-10 11:40:59 +0000

    Updates tests for the OptionalExtras component

--- __tests__/OptionalExtras.test.tsx
+++ __tests__/OptionalExtras.test.tsx
@@ -1,6 +1,7 @@
 import {
   render,
   screen,
+  waitFor,
   waitForElementToBeRemoved,
 } from '@testing-library/react';
 import '@testing-library/jest-dom';
@@ -9,7 +10,7 @@ import OptionalExtras from '../components/optionalExtras';
 
 const noop = () => {};
 
-test('It should initially display three buttons with all optional extra content hidden', () => {
+test('It should initially display four buttons with all optional extra content hidden', () => {
   render(
     <OptionalExtras
       highlightContent="Highlight!"
@@ -18,7 +19,7 @@ test('It should initially display three buttons with all optional extra content 
       retrieveColorFilterSelected={noop}
     />,
   );
-
+  expect(screen.getAllByRole('button')).toHaveLength(4);
   expect(
     screen.getByRole('button', { name: 'Highlights' }),
   ).toBeInTheDocument();
@@ -26,10 +27,14 @@ test('It should initially display three buttons with all optional extra content 
   expect(
     screen.getByRole('button', { name: 'Definitions' }),
   ).toBeInTheDocument();
+  expect(
+    screen.getByRole('button', { name: 'Visual aids' }),
+  ).toBeInTheDocument();
 
   expect(screen.queryByText('Highlight!')).not.toBeInTheDocument();
   expect(screen.queryByText('Diagram!')).not.toBeInTheDocument();
   expect(screen.queryByText('Definition!')).not.toBeInTheDocument();
+  expect(screen.queryByText('Reading guide')).not.toBeInTheDocument();
 });
 
 test('It should toggle whether the highlights content is shown when the highlights button is clicked', async () => {
@@ -83,6 +88,120 @@ test('It should toggle whether the definitions content is shown when the definit
   await waitForElementToBeRemoved(() => screen.queryByText('Definition!'));
 });
 
+test('It should toggle whether the visual aids options are shown when the Visual aids button is clicked', async () => {
+  render(
+    <OptionalExtras
+      highlightContent="Highlight!"
+      diagramContent="Diagram!"
+      definitionsContent="Definition!"
+      retrieveColorFilterSelected={noop}
+    />,
+  );
+
+  const visualAidsButton = screen.getByRole('button', { name: 'Visual aids' });
+  userEvent.click(visualAidsButton);
+  await screen.findByText('Reading guide');
+  userEvent.click(visualAidsButton);
+  await waitForElementToBeRemoved(() => screen.queryByText('Reading guide'));
+});
+
+test('When visual aids are open, we can toggle the reading guide toggle', async () => {
+  render(
+    <OptionalExtras
+      highlightContent="Highlight!"
+      diagramContent="Diagram!"
+      definitionsContent="Definition!"
+      retrieveColorFilterSelected={noop}
+    />,
+  );
+
+  const visualAidsButton = screen.getByRole('button', { name: 'Visual aids' });
+  userEvent.click(visualAidsButton);
+  await screen.findByText('Reading guide');
+  const readingGuideToggle = screen.getByRole('checkbox', {
+    name: 'Reading guide',
+  });
+  userEvent.click(readingGuideToggle);
+  await screen.findByText(/Drag me around/i);
+  userEvent.click(readingGuideToggle);
+  await waitForElementToBeRemoved(() => screen.queryByText(/Drag me around/i));
+});
+
+test('When the reading guide is displayed, we can click the cross to remove it', async () => {
+  render(
+    <OptionalExtras
+      highlightContent="Highlight!"
+      diagramContent="Diagram!"
+      definitionsContent="Definition!"
+      retrieveColorFilterSelected={noop}
+    />,
+  );
+
+  const visualAidsButton = screen.getByRole('button', { name: 'Visual aids' });
+  userEvent.click(visualAidsButton);
+  await screen.findByText('Reading guide');
+  const readingGuideToggle = screen.getByRole('checkbox', {
+    name: 'Reading guide',
+  });
+  userEvent.click(readingGuideToggle);
+  await screen.findByText(/Drag me around/i);
+  const closeReadingGuideButton = screen.getByRole('button', { name: '' });
+  userEvent.click(closeReadingGuideButton);
+  await waitForElementToBeRemoved(() => screen.queryByText(/Drag me around/i));
+});
+
+test('When visual aids are open, we can choose a colour filter', async () => {
+  render(
+    <OptionalExtras
+      highlightContent="Highlight!"
+      diagramContent="Diagram!"
+      definitionsContent="Definition!"
+      retrieveColorFilterSelected={noop}
+    />,
+  );
+
+  const visualAidsButton = screen.getByRole('button', { name: 'Visual aids' });
+  userEvent.click(visualAidsButton);
+  await screen.findByText('Colour filters');
+
+  expect(screen.getAllByRole('radio')).toHaveLength(4);
+  const noneRadioButton = screen.getByRole('radio', {
+    name: 'None',
+  });
+  const whiteRadioButton = screen.getByRole('radio', { name: 'White' });
+  const yellowRadioButton = screen.getByRole('radio', { name: 'Yellow' });
+  const blueRadioButton = screen.getByRole('radio', { name: 'Blue' });
+
+  expect(noneRadioButton).toBeChecked();
+  expect(whiteRadioButton).not.toBeChecked();
+  expect(yellowRadioButton).not.toBeChecked();
+  expect(blueRadioButton).not.toBeChecked();
+
+  userEvent.click(whiteRadioButton);
+  await waitFor(() => {
+    expect(whiteRadioButton).toBeChecked();
+  });
+  expect(noneRadioButton).not.toBeChecked();
+  expect(yellowRadioButton).not.toBeChecked();
+  expect(blueRadioButton).not.toBeChecked();
+
+  userEvent.click(yellowRadioButton);
+  await waitFor(() => {
+    expect(yellowRadioButton).toBeChecked();
+  });
+  expect(noneRadioButton).not.toBeChecked();
+  expect(whiteRadioButton).not.toBeChecked();
+  expect(blueRadioButton).not.toBeChecked();
+
+  userEvent.click(blueRadioButton);
+  await waitFor(() => {
+    expect(blueRadioButton).toBeChecked();
+  });
+  expect(noneRadioButton).not.toBeChecked();
+  expect(yellowRadioButton).not.toBeChecked();
+  expect(whiteRadioButton).not.toBeChecked();
+});
+
 test('It should hide highlights and show diagram when highlights are visible but the diagram button is clicked', async () => {
   render(
     <OptionalExtras
@@ -119,6 +238,25 @@ test('It should hide highlights and show definitions when highlights are visible
   userEvent.click(definitionButton);
   await waitForElementToBeRemoved(() => screen.queryByText('Highlight!'));
   expect(screen.getByText('Definition!')).toBeInTheDocument();
+});
+
+test('It should hide highlights and show visual aids when highlights are visible but the visual aids button is clicked', async () => {
+  render(
+    <OptionalExtras
+      highlightContent="Highlight!"
+      diagramContent="Diagram!"
+      definitionsContent="Definition!"
+      retrieveColorFilterSelected={noop}
+    />,
+  );
+  const highlightButton = screen.getByRole('button', { name: 'Highlights' });
+  const visualAidsButton = screen.getByRole('button', { name: 'Visual aids' });
+  userEvent.click(highlightButton);
+  await screen.findByText('Highlight!');
+  expect(screen.queryByText('Reading guide')).not.toBeInTheDocument();
+  userEvent.click(visualAidsButton);
+  await waitForElementToBeRemoved(() => screen.queryByText('Highlight!'));
+  expect(screen.getByText('Reading guide')).toBeInTheDocument();
 });
 
 test('It should hide diagram and show definitions when diagram is visible but the definitions button is clicked', async () => {
@@ -159,6 +297,25 @@ test('It should hide diagram and show highlights when diagram is visible but the
   expect(screen.getByText('Highlight!')).toBeInTheDocument();
 });
 
+test('It should hide diagram and show visual aids when diagram is visible but the visual aids button is clicked', async () => {
+  render(
+    <OptionalExtras
+      highlightContent="Highlight!"
+      diagramContent="Diagram!"
+      definitionsContent="Definition!"
+      retrieveColorFilterSelected={noop}
+    />,
+  );
+  const diagramButton = screen.getByRole('button', { name: 'Diagram' });
+  const visualAidsButton = screen.getByRole('button', { name: 'Visual aids' });
+  userEvent.click(diagramButton);
+  await screen.findByText('Diagram!');
+  expect(screen.queryByText('Reading guide')).not.toBeInTheDocument();
+  userEvent.click(visualAidsButton);
+  await waitForElementToBeRemoved(() => screen.queryByText('Diagram!'));
+  expect(screen.getByText('Reading guide')).toBeInTheDocument();
+});
+
 test('It should hide definitions and show highlights when definitions are visible but the highlights button is clicked', async () => {
   render(
     <OptionalExtras
@@ -195,4 +352,80 @@ test('It should hide definitions and show diagram when definitions are visible b
   userEvent.click(diagramButton);
   await waitForElementToBeRemoved(() => screen.queryByText('Definition!'));
   expect(screen.getByText('Diagram!')).toBeInTheDocument();
+});
+
+test('It should hide definitions and show visual aids when definitions are visible but the visual aids button is clicked', async () => {
+  render(
+    <OptionalExtras
+      highlightContent="Highlight!"
+      diagramContent="Diagram!"
+      definitionsContent="Definition!"
+      retrieveColorFilterSelected={noop}
+    />,
+  );
+  const definitionButton = screen.getByRole('button', { name: 'Definitions' });
+  const visualAidsButton = screen.getByRole('button', { name: 'Visual aids' });
+  userEvent.click(definitionButton);
+  await screen.findByText('Definition!');
+  expect(screen.queryByText('Reading guide')).not.toBeInTheDocument();
+  userEvent.click(visualAidsButton);
+  await waitForElementToBeRemoved(() => screen.queryByText('Definition!'));
+  expect(screen.getByText('Reading guide')).toBeInTheDocument();
+});
+
+test('It should hide visual aids and show highlights when visual aids are visible but the highlights button is clicked', async () => {
+  render(
+    <OptionalExtras
+      highlightContent="Highlight!"
+      diagramContent="Diagram!"
+      definitionsContent="Definition!"
+      retrieveColorFilterSelected={noop}
+    />,
+  );
+  const visualAidsButton = screen.getByRole('button', { name: 'Visual aids' });
+  const highlightButton = screen.getByRole('button', { name: 'Highlights' });
+  userEvent.click(visualAidsButton);
+  await screen.findByText('Reading guide');
+  expect(screen.queryByText('Highlight!')).not.toBeInTheDocument();
+  userEvent.click(highlightButton);
+  await waitForElementToBeRemoved(() => screen.queryByText('Reading guide'));
+  expect(screen.getByText('Highlight!')).toBeInTheDocument();
+});
+
+test('It should hide visual aids and show diagram when visual aids are visible but the diagram button is clicked', async () => {
+  render(
+    <OptionalExtras
+      highlightContent="Highlight!"
+      diagramContent="Diagram!"
+      definitionsContent="Definition!"
+      retrieveColorFilterSelected={noop}
+    />,
+  );
+  const visualAidsButton = screen.getByRole('button', { name: 'Visual aids' });
+  const diagramButton = screen.getByRole('button', { name: 'Diagram' });
+  userEvent.click(visualAidsButton);
+  await screen.findByText('Reading guide');
+  expect(screen.queryByText('Diagram!')).not.toBeInTheDocument();
+  userEvent.click(diagramButton);
+  await waitForElementToBeRemoved(() => screen.queryByText('Reading guide'));
+  expect(screen.getByText('Diagram!')).toBeInTheDocument();
+});
+
+test('It should hide visual aids and show definitions when visual aids are visible but the definitions button is clicked', async () => {
+  render(
+    <OptionalExtras
+      highlightContent="Highlight!"
+      diagramContent="Diagram!"
+      definitionsContent="Definition!"
+      retrieveColorFilterSelected={noop}
+    />,
+  );
+  const visualAidsButton = screen.getByRole('button', { name: 'Visual aids' });
+  const definitionsButton = screen.getByRole('button', { name: 'Definitions' });
+  userEvent.click(visualAidsButton);
+  await screen.findByText('Reading guide');
+  expect(screen.queryByText('Definition!')).not.toBeInTheDocument();
+  userEvent.click(definitionsButton);
+  await waitForElementToBeRemoved(() => screen.queryByText('Reading guide'));
+  expect(screen.getByText('Definition!')).toBeInTheDocument();
 });
