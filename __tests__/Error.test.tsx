a77cad35967532f14da0a5054c2129fe89894d17
Beth Gooding <beth.gooding@hotmail.com>
2022-11-10 20:17:49 +0000

    Removes unused imports from the testing files

--- __tests__/Error.test.tsx
+++ __tests__/Error.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Error from '../pages/error';
 
