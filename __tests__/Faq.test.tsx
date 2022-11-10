bdfa70fdbd261667f8a20f3676ad26fccc33b076
Beth Gooding <beth.gooding@hotmail.com>
2022-11-10 10:47:59 +0000

    Adds a test for the FAQ page

---
+++ __tests__/Faq.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import FAQs from '../pages/faqs';
+
+test("It should render a heading of 'FAQs'", () => {
+  render(<FAQs />);
+  expect(screen.getByRole('heading', { name: 'FAQs' }));
+});
