f76c83d50e2bc0306177af811db67c2b2400204f
Beth Gooding <beth.gooding@hotmail.com>
2022-11-10 14:14:23 +0000

    Updates test cases for ExampleQuestion page

--- __tests__/ExampleQuestion.test.tsx
+++ __tests__/ExampleQuestion.test.tsx
@@ -1,4 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import ExampleQuestion from '../pages/example-question';
 
@@ -7,4 +12,20 @@ test('The example question page has the expected title', () => {
   expect(
     screen.getByRole('heading', { name: /Question 1/i }),
   ).toBeInTheDocument();
+});
+
+test('Clicking the submit button should cause an overlay to appear', async () => {
+  render(<ExampleQuestion />);
+  const submitButton = screen.getByRole('button', { name: 'Submit' });
+  userEvent.click(submitButton);
+  await screen.findByText(
+    /In your real test this will take you to the break page./i,
+  );
+  const overlayButton = screen.getByRole('button', { name: 'Okay' });
+  userEvent.click(overlayButton);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(
+      /In your real test this will take you to the break page./i,
+    ),
+  );
 });
