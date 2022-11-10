5b91429547ff9d2152ce069a3ce4682c4f7d7578
Beth Gooding <beth.gooding@hotmail.com>
2022-11-10 17:11:07 +0000

    Updates unit tests for the QuestionDisplay components

--- __tests__/QuestionDisplay.test.tsx
+++ __tests__/QuestionDisplay.test.tsx
@@ -4,26 +4,27 @@ import '@testing-library/jest-dom';
 import QuestionDisplay from '../components/questionDisplay';
 
 const noop = jest.fn();
+const QuestionDisplayComponent = (
+  <QuestionDisplay
+    id={1}
+    question="Question?"
+    answer="Answer A"
+    resA="Answer A"
+    resB="Answer B"
+    resC="Answer C"
+    resD="Answer D"
+    highlight="highlight"
+    image="imageLink"
+    definitions="definition"
+    timeLimit={60}
+    handleSubmitOnClick={noop}
+    clockIsAnimated={true}
+    setClockIsAnimated={noop}
+  />
+);
 
 test('The question displays the expected question and answer props', () => {
-  render(
-    <QuestionDisplay
-      id={1}
-      question="Question?"
-      answer="Answer A"
-      resA="Answer A"
-      resB="Answer B"
-      resC="Answer C"
-      resD="Answer D"
-      highlight="highlight"
-      image="imageLink"
-      definitions="definition"
-      timeLimit={60}
-      handleSubmitOnClick={noop}
-      clockIsAnimated={true}
-      setClockIsAnimated={noop}
-    />,
-  );
+  render(QuestionDisplayComponent);
   expect(
     screen.getByRole('heading', { name: /Question 1/i }),
   ).toBeInTheDocument();
@@ -35,24 +36,7 @@ test('The question displays the expected question and answer props', () => {
 });
 
 test('The radio element that is clicked becomes checked', async () => {
-  render(
-    <QuestionDisplay
-      id={1}
-      question="Question?"
-      answer="Answer A"
-      resA="Answer A"
-      resB="Answer B"
-      resC="Answer C"
-      resD="Answer D"
-      highlight="highlight"
-      image="imageLink"
-      definitions="definition"
-      timeLimit={60}
-      handleSubmitOnClick={noop}
-      clockIsAnimated={true}
-      setClockIsAnimated={noop}
-    />,
-  );
+  render(QuestionDisplayComponent);
 
   const radioA = screen.getByRole('radio', { name: 'Answer A' });
   const radioB = screen.getByRole('radio', { name: 'Answer B' });
@@ -87,4 +71,121 @@ test('The radio element that is clicked becomes checked', async () => {
   expect(radioA).not.toBeChecked();
   expect(radioB).not.toBeChecked();
   expect(radioC).not.toBeChecked();
+});
+
+test('It should render three buttons that can be used to customise font size', async () => {
+  render(QuestionDisplayComponent);
+  const questionText = screen.getByText('Question?');
+  expect(questionText).toHaveStyle({ 'font-size': '1rem' });
+
+  const decreaseFontSizeButton = screen.getByRole('button', {
+    name: 'reduce font size',
+  });
+  const resetFontSizeButton = screen.getByRole('button', {
+    name: 'reset font size',
+  });
+  const increaseFontSizeButton = screen.getByRole('button', {
+    name: 'increase font size',
+  });
+
+  userEvent.click(decreaseFontSizeButton);
+  await waitFor(() => {
+    expect(questionText).toHaveStyle({
+      'font-size': '0.75rem',
+    });
+  });
+
+  userEvent.click(increaseFontSizeButton);
+  await waitFor(() => {
+    expect(questionText).toHaveStyle({
+      'font-size': '1rem',
+    });
+  });
+
+  userEvent.click(increaseFontSizeButton);
+  await waitFor(() => {
+    expect(questionText).toHaveStyle({
+      'font-size': '1.25rem',
+    });
+  });
+
+  userEvent.click(increaseFontSizeButton);
+  await waitFor(() => {
+    expect(questionText).toHaveStyle({
+      'font-size': '1.5rem',
+    });
+  });
+
+  userEvent.click(resetFontSizeButton);
+  await waitFor(() => {
+    expect(questionText).toHaveStyle({
+      'font-size': '1rem',
+    });
+  });
+});
+
+test('It should disable the reduce fontsize button when the fontsize is less than 0.75rem', async () => {
+  render(QuestionDisplayComponent);
+
+  const questionText = screen.getByText('Question?');
+  expect(questionText).toHaveStyle({ 'font-size': '1rem' });
+
+  const decreaseFontSizeButton = screen.getByRole('button', {
+    name: 'reduce font size',
+  });
+
+  userEvent.click(decreaseFontSizeButton);
+  userEvent.click(decreaseFontSizeButton);
+  await waitFor(() => {
+    expect(questionText).toHaveStyle({
+      'font-size': '0.5rem',
+    });
+  });
+  expect(decreaseFontSizeButton).toBeDisabled();
+});
+
+test('It should disable the increase fontsize button when the fontsize is more than 2.5rem', async () => {
+  render(QuestionDisplayComponent);
+
+  const questionText = screen.getByText('Question?');
+  expect(questionText).toHaveStyle({ 'font-size': '1rem' });
+
+  const increaseFontSizeButton = screen.getByRole('button', {
+    name: 'increase font size',
+  });
+
+  userEvent.click(increaseFontSizeButton);
+  userEvent.click(increaseFontSizeButton);
+  userEvent.click(increaseFontSizeButton);
+  userEvent.click(increaseFontSizeButton);
+  userEvent.click(increaseFontSizeButton);
+  userEvent.click(increaseFontSizeButton);
+  userEvent.click(increaseFontSizeButton);
+
+  await waitFor(() => {
+    expect(questionText).toHaveStyle({
+      'font-size': '2.75rem',
+    });
+  });
+  expect(increaseFontSizeButton).toBeDisabled();
+});
+
+test('It should retrieve the colour filters and change the styling accordingly', async () => {
+  render(QuestionDisplayComponent);
+  userEvent.click(screen.getByRole('button', { name: 'Visual aids' }));
+  await waitFor(() => {
+    expect(screen.getByText('Colour filters')).toBeInTheDocument();
+  });
+  userEvent.click(screen.getByRole('radio', { name: 'Blue' }));
+  await waitFor(() => {
+    expect(screen.getByTestId('backgroundColourDiv')).toHaveClass(
+      'p-4 max-w-4xl flex-col justify-center mx-auto bg-blue',
+    );
+  });
+  userEvent.click(screen.getByRole('radio', { name: 'Yellow' }));
+  await waitFor(() => {
+    expect(screen.getByTestId('backgroundColourDiv')).toHaveClass(
+      'p-4 max-w-4xl flex-col justify-center mx-auto bg-yellow',
+    );
+  });
 });
