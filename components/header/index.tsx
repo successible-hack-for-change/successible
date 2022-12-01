092a4ebf79498599d56f1dddbfb183b1a93cc80f
IssyYeung <issyyeung@hotmail.co.uk>
2022-12-01 19:11:00 +0000

    Mobile view nav bar

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { Navbar, Button } from '@blueprintjs/core';
+import React, { useState } from 'react';
+import { Navbar, Button, Drawer, Position } from '@blueprintjs/core';
 import Link from 'next/link';
 import Image from 'next/image';
 
 const Header = (): JSX.Element => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = (state: boolean) => {
+    setIsMenuOpen(!state);
+    console.log(!state);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header>
-      <Navbar>
+      <Navbar className="hidden md:block">
         <Navbar.Group>
           <Image
             src="/successible-logo.svg"
@@ -34,6 +45,56 @@ const Header = (): JSX.Element => {
           </Link>
         </Navbar.Group>
       </Navbar>
+      {/* ========================================================================================== */}
+      <Navbar className="md:hidden">
+        <Navbar.Group>
+          <Button
+            className="bp4-minimal"
+            icon="menu"
+            onClick={() => toggleMenu(isMenuOpen)}
+          />
+          <Navbar.Divider />
+          <Image
+            src="/successible-logo.svg"
+            alt="Successible logo"
+            height={30}
+            width={30}
+          />
+          <Navbar.Heading className="pl-2">Successible</Navbar.Heading>
+        </Navbar.Group>
+      </Navbar>
+      <Drawer
+        isOpen={isMenuOpen}
+        onClose={closeMenu}
+        position={(Position.LEFT = Position.LEFT)}
+        size={'200px'}
+        canEscapeKeyClose
+        canOutsideClickClose
+        isCloseButtonShown
+        title="Menu"
+        hasBackdrop={false}
+      >
+        <Link href="/">
+          <Button className="bp4-minimal mt-2" icon="home" text="Home" />
+        </Link>
+        <Link href="/instructions">
+          <Button
+            className="bp4-minimal mt-2"
+            icon="manual"
+            text="Instructions"
+          />
+        </Link>
+        <Link href="/example-question">
+          <Button
+            className="bp4-minimal mt-2"
+            icon="learning"
+            text="Example question"
+          />
+        </Link>
+        <Link href="/start-test">
+          <Button className="bp4-minimal mt-2" icon="play" text="Start test" />
+        </Link>
+      </Drawer>
     </header>
   );
 };
